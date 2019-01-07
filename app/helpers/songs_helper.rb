33040a705be5624f7ee045d1392a9701061eb50c
Judy McGoldrick <jjeanius@verizon.net>
2019-01-07 02:59:56 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,8 +1,3 @@
 module SongsHelper
 
-  def navigate
-    @song = Song.create(params[:name, :artist_id, :genre_id])
-    @artist = Artist.create(params[:name, :bio])
-    @genre = Genre.create(params[:name])
-  end
 end
