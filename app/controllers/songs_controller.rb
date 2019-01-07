42e0d9f9b21c326e22fe448b30b0b3bc53ac6802
Judy McGoldrick <jjeanius@verizon.net>
2019-01-07 02:14:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,10 +14,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new
-    @song.name = params[:name]
-    @song.artist_id = params[artist_id]
-    @song.genre_id = params[genre_id]
+    @song = Song.new(song_params[:name, :artist_id, :genre_id])
     @song.save
     redirect_to song_path(@song)
   end
